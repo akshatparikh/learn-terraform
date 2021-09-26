a617633f406d6850a6c0010c8317f08366feeee5
Akshat Parikh <akshat@wiredsolutions.ca>
2021-09-26 15:26:08 -0400

    Added config files for terraform with gcp

---
+++ main.tf
@@ -0,0 +1,36 @@
+terraform {
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "3.5.0"
+    }
+  }
+}
+
+provider "google" {
+  credentials = file("wordpair-306618-4d45eee5722f.json")
+  project     = "wordpair-306618"
+  region  = var.region
+  zone    = var.zone
+}
+
+resource "google_compute_network" "vpc_network" {
+  name = "terraform-network"
+}
+
+resource "google_compute_instance" "vm_instance" {
+  name         = "terraform-network"
+  machine_type = "f1-micro"
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-9"
+    }
+  }
+
+  network_interface {
+    network = google_compute_network.vpc_network.name
+    access_config {
+    }
+  }
+}
---
+++ terraform.tfvars
@@ -0,0 +1,2 @@
+project                  = "wordpair-306618"
+credentials_file         = "wordpair-306618-4d45eee5722f.json"
